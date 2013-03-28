429137a707233ebeb688a42d9d5267d4386b2e1d
TygerStripe <tiger@tyger.org>
2013-03-28 16:12:33 -0700

    Removed the need for the md5 hash, and verified function changes other ways

--- targets.user.js
+++ targets.user.js
@@ -30,33 +30,11 @@ if ('undefined' == typeof __PAGE_SCOPE_RUN__) {
   // an anonymous wrapper.
   return;
 }
-// For some reason I couldn't include CryptoJS via the same method datatables is included and have it work.
-// I just copied all the code below to work around that issue.
-//BEGIN CryptoJS
-/*
-CryptoJS v3.0.2
-code.google.com/p/crypto-js
-(c) 2009-2012 by Jeff Mott. All rights reserved.
-code.google.com/p/crypto-js/wiki/License
-*/
-var CryptoJS=CryptoJS||function(o,q){var l={},m=l.lib={},n=m.Base=function(){function a(){}return{extend:function(e){a.prototype=this;var c=new a;e&&c.mixIn(e);c.$super=this;return c},create:function(){var a=this.extend();a.init.apply(a,arguments);return a},init:function(){},mixIn:function(a){for(var c in a)a.hasOwnProperty(c)&&(this[c]=a[c]);a.hasOwnProperty("toString")&&(this.toString=a.toString)},clone:function(){return this.$super.extend(this)}}}(),j=m.WordArray=n.extend({init:function(a,e){a=
-this.words=a||[];this.sigBytes=e!=q?e:4*a.length},toString:function(a){return(a||r).stringify(this)},concat:function(a){var e=this.words,c=a.words,d=this.sigBytes,a=a.sigBytes;this.clamp();if(d%4)for(var b=0;b<a;b++)e[d+b>>>2]|=(c[b>>>2]>>>24-8*(b%4)&255)<<24-8*((d+b)%4);else if(65535<c.length)for(b=0;b<a;b+=4)e[d+b>>>2]=c[b>>>2];else e.push.apply(e,c);this.sigBytes+=a;return this},clamp:function(){var a=this.words,e=this.sigBytes;a[e>>>2]&=4294967295<<32-8*(e%4);a.length=o.ceil(e/4)},clone:function(){var a=
-n.clone.call(this);a.words=this.words.slice(0);return a},random:function(a){for(var e=[],c=0;c<a;c+=4)e.push(4294967296*o.random()|0);return j.create(e,a)}}),k=l.enc={},r=k.Hex={stringify:function(a){for(var e=a.words,a=a.sigBytes,c=[],d=0;d<a;d++){var b=e[d>>>2]>>>24-8*(d%4)&255;c.push((b>>>4).toString(16));c.push((b&15).toString(16))}return c.join("")},parse:function(a){for(var b=a.length,c=[],d=0;d<b;d+=2)c[d>>>3]|=parseInt(a.substr(d,2),16)<<24-4*(d%8);return j.create(c,b/2)}},p=k.Latin1={stringify:function(a){for(var b=
-a.words,a=a.sigBytes,c=[],d=0;d<a;d++)c.push(String.fromCharCode(b[d>>>2]>>>24-8*(d%4)&255));return c.join("")},parse:function(a){for(var b=a.length,c=[],d=0;d<b;d++)c[d>>>2]|=(a.charCodeAt(d)&255)<<24-8*(d%4);return j.create(c,b)}},h=k.Utf8={stringify:function(a){try{return decodeURIComponent(escape(p.stringify(a)))}catch(b){throw Error("Malformed UTF-8 data");}},parse:function(a){return p.parse(unescape(encodeURIComponent(a)))}},b=m.BufferedBlockAlgorithm=n.extend({reset:function(){this._data=j.create();
-this._nDataBytes=0},_append:function(a){"string"==typeof a&&(a=h.parse(a));this._data.concat(a);this._nDataBytes+=a.sigBytes},_process:function(a){var b=this._data,c=b.words,d=b.sigBytes,f=this.blockSize,i=d/(4*f),i=a?o.ceil(i):o.max((i|0)-this._minBufferSize,0),a=i*f,d=o.min(4*a,d);if(a){for(var h=0;h<a;h+=f)this._doProcessBlock(c,h);h=c.splice(0,a);b.sigBytes-=d}return j.create(h,d)},clone:function(){var a=n.clone.call(this);a._data=this._data.clone();return a},_minBufferSize:0});m.Hasher=b.extend({init:function(){this.reset()},
-reset:function(){b.reset.call(this);this._doReset()},update:function(a){this._append(a);this._process();return this},finalize:function(a){a&&this._append(a);this._doFinalize();return this._hash},clone:function(){var a=b.clone.call(this);a._hash=this._hash.clone();return a},blockSize:16,_createHelper:function(a){return function(b,c){return a.create(c).finalize(b)}},_createHmacHelper:function(a){return function(b,c){return f.HMAC.create(a,c).finalize(b)}}});var f=l.algo={};return l}(Math);
-(function(o){function q(b,f,a,e,c,d,g){b=b+(f&a|~f&e)+c+g;return(b<<d|b>>>32-d)+f}function l(b,f,a,e,c,d,g){b=b+(f&e|a&~e)+c+g;return(b<<d|b>>>32-d)+f}function m(b,f,a,e,c,d,g){b=b+(f^a^e)+c+g;return(b<<d|b>>>32-d)+f}function n(b,f,a,e,c,d,g){b=b+(a^(f|~e))+c+g;return(b<<d|b>>>32-d)+f}var j=CryptoJS,k=j.lib,r=k.WordArray,k=k.Hasher,p=j.algo,h=[];(function(){for(var b=0;64>b;b++)h[b]=4294967296*o.abs(o.sin(b+1))|0})();p=p.MD5=k.extend({_doReset:function(){this._hash=r.create([1732584193,4023233417,
-2562383102,271733878])},_doProcessBlock:function(b,f){for(var a=0;16>a;a++){var e=f+a,c=b[e];b[e]=(c<<8|c>>>24)&16711935|(c<<24|c>>>8)&4278255360}for(var e=this._hash.words,c=e[0],d=e[1],g=e[2],i=e[3],a=0;64>a;a+=4)16>a?(c=q(c,d,g,i,b[f+a],7,h[a]),i=q(i,c,d,g,b[f+a+1],12,h[a+1]),g=q(g,i,c,d,b[f+a+2],17,h[a+2]),d=q(d,g,i,c,b[f+a+3],22,h[a+3])):32>a?(c=l(c,d,g,i,b[f+(a+1)%16],5,h[a]),i=l(i,c,d,g,b[f+(a+6)%16],9,h[a+1]),g=l(g,i,c,d,b[f+(a+11)%16],14,h[a+2]),d=l(d,g,i,c,b[f+a%16],20,h[a+3])):48>a?(c=
-m(c,d,g,i,b[f+(3*a+5)%16],4,h[a]),i=m(i,c,d,g,b[f+(3*a+8)%16],11,h[a+1]),g=m(g,i,c,d,b[f+(3*a+11)%16],16,h[a+2]),d=m(d,g,i,c,b[f+(3*a+14)%16],23,h[a+3])):(c=n(c,d,g,i,b[f+3*a%16],6,h[a]),i=n(i,c,d,g,b[f+(3*a+7)%16],10,h[a+1]),g=n(g,i,c,d,b[f+(3*a+14)%16],15,h[a+2]),d=n(d,g,i,c,b[f+(3*a+5)%16],21,h[a+3]));e[0]=e[0]+c|0;e[1]=e[1]+d|0;e[2]=e[2]+g|0;e[3]=e[3]+i|0},_doFinalize:function(){var b=this._data,f=b.words,a=8*this._nDataBytes,e=8*b.sigBytes;f[e>>>5]|=128<<24-e%32;f[(e+64>>>9<<4)+14]=(a<<8|a>>>
-24)&16711935|(a<<24|a>>>8)&4278255360;b.sigBytes=4*(f.length+1);this._process();b=this._hash.words;for(f=0;4>f;f++)a=b[f],b[f]=(a<<8|a>>>24)&16711935|(a<<24|a>>>8)&4278255360}});j.MD5=k._createHelper(p);j.HmacMD5=k._createHmacHelper(p)})(Math);
-//END CryptoJS
 
-//86cbd04199d932346d107e6afb6dd562 is the MD5 of the current version of function Zc.
-//Since I just want to add a single line to the function, I have to ensure nothing has changed from the version I know.
-var md5 = CryptoJS.MD5(String(Zc.prototype.constructor))
-if (md5 != "86cbd04199d932346d107e6afb6dd562")
+if (String(Zc.prototype.constructor).indexOf("(a, b, c, d, e, f)") < 0 ||
+    String(Zc.prototype.constructor).indexOf("type:\"POST\", url:\"/rpc/\"") < 0)
 {
-  alert("NianticOps changed something, please get a new version\n\nCurrent version md5 = " + md5);
+  alert("NianticOps changed something, please get a new version");
   return;
 }
 
@@ -397,160 +375,7 @@ function gatherCoordinates() {
   document.querySelector("#graphCoords").select();
 }
 
-
-//This *SHOULD* work, but it doesn't. I think it has something to do with the greasemonkey/userscript wrapper.
-/*
-window.Zc = new Function (
-  "a", "b", "c", "d", "e",
-  String(Zc.prototype.constructor)
-    .replace("success:function(a) {", "success:function(a) {\n    CollectPortalInfo(a)")
-    .replace(/^function[^{]+{/i,"")  // remove everything up to and including the first curly bracket
-    .replace(/}[^}]*$/i, "")  // remove last curly bracket and everything after
-);
-*/
-
-//Since I can't just insert the line I want, I have to overwrite the entire function with the last known function + the included line.
-//This is why I break out if the function changes from my known version.
-/*
-Old function S, retired on 2013/01/29 - MD5 was 14201404787536b5f24a9867d7e981fb
-window.S = function (a, b, c, d, e) {
-  c.method = b;
-  var f = t(a.Fc, a, b, e), g = t(a.cd, a, b, d), h = t(a.jd, a);
-  _gaq.push(["_trackEvent", "RPC", b]);
-  c = $.ajax({type:"POST", url:"/rpc/" + b, dataType:"json", contentType:"application/json; charset=utf-8", data:JSON.stringify(c), Jd:function() {
-    f()
-  }, success:function(a) {
-    CollectPortalInfo(a)
-    g(a)
-  }, error:function() {
-    _gaq.push(["_trackEvent", "RPC_error", b])
-  }, complete:function(a) {
-    h(b, a)
-  }});
-  Mc(a, b).push(c)
-}
-*/
-/*
-Old function S, retired on 2013/02/01 - MD5 was 7dd1bbbbae015515411974441bbf4b92
-window.S = function (a, b, c, d, e) {
-  c.method = b;
-  var f = t(a.Fc, a, b, e), g = t(a.bd, a, b, d), h = t(a.gd, a);
-  _gaq.push(["_trackEvent", "RPC", b]);
-  c = $.ajax({type:"POST", url:"/rpc/" + b, dataType:"json", contentType:"application/json; charset=utf-8", data:JSON.stringify(c), Id:function() {
-    f()
-  }, success:function(a) {
-    CollectPortalInfo(a)
-    g(a)
-  }, error:function() {
-    _gaq.push(["_trackEvent", "RPC_error", b])
-  }, complete:function(a) {
-    h(b, a)
-  }});
-  Mc(a, b).push(c)
-}
-*/
-
-/*
-Old function S, retired on 2013-02-27 - MD5 was 8f3225281c7d9928f502f2afa9cda934
-window.S = function (a, b, c, d, e, f) {
-  c.method = b;
-  var g = t(a.Fc, a, b, e), h = t(a.bd, a, b, d), i = t(a.gd, a);
-  _gaq.push(["_trackEvent", "RPC", b]);
-  c = $.ajax({type:"POST", url:"/rpc/" + b, dataType:"json", contentType:"application/json; charset=utf-8", data:JSON.stringify(c), Id:function() {
-    g()
-  }, success:function(a) {
-    CollectPortalInfo(a)
-    h(a)
-  }, error:function(a, c, d) {
-    _gaq.push(["_trackEvent", "RPC_error", b]);
-    f && f({error:c || "unknown", httpError:d})
-  }, complete:function(a) {
-    i(b, a)
-  }});
-  Mc(a, b).push(c)
-}
-*/
-
-/*
-Old function S, retired on 2013-03-06 - MD5 was 378c25a3a9c859619c0d24047b8d2cbc
-window.S = function(a, b, c, d, e, f) {
-  c.method = b;
-  var g = t(a.Hc, a, b, e), h = t(a.ed, a, b, d), i = t(a.ld, a);
-  _gaq.push(["_trackEvent", "RPC", b]);
-  c = $.ajax({type:"POST", url:"/rpc/" + b, dataType:"json", contentType:"application/json; charset=utf-8", data:JSON.stringify(c), Ld:function() {
-    g()
-  }, success:function(a) {
-    CollectPortalInfo(a)
-    h(a)
-  }, error:function(a, c, d) {
-    _gaq.push(["_trackEvent", "RPC_error", b]);
-    f && f({error:c || "unknown", httpError:d})
-  }, complete:function(a) {
-    i(b, a)
-  }});
-  Oc(a, b).push(c)
-}
-*/
-
-/*
-Old function S, retired on 2013-03-19 - MD5 was a835fdcca8f81b01e745f3bf905107b7
-window.S = function(a, b, c, d, e, f) {
-  c.method = b;
-  var g = u(a.Dc, a, b, e), h = u(a.Yc, a, b, d), i = u(a.ad, a);
-  _gaq.push(["_trackEvent", "RPC", b]);
-  c = $.ajax({type:"POST", url:"/rpc/" + b, dataType:"json", contentType:"application/json; charset=utf-8", data:JSON.stringify(c), wd:function() {
-    g()
-  }, success:function(a) {
-    CollectPortalInfo(a)
-    h(a)
-  }, error:function(a, c) {
-    _gaq.push(["_trackEvent", "RPC_error", b]);
-    f && f({error:c || "unknown", respStatus:a.status})
-  }, complete:function(a) {
-    i(b, a)
-  }});
-  Qc(a, b).push(c)
-}
-*/
-
-/*
-Old function Zc, retired on 2013-03-28 - MD5 was 005248a11dea1df42d55141aa5907f55
-window.Zc = function(a, b, c, d, e, f) {
-  c.method = b;
-  var g = t(a.Gc, a, b, e), h = t(a.cd, a, b, d), j = t(a.fd, a);
-  _gaq.push(["_trackEvent", "RPC", b]);
-  c = $.ajax({type:"POST", url:"/rpc/" + b, dataType:"json", contentType:"application/json; charset=utf-8", data:JSON.stringify(c), zd:function() {
-    g()
-  }, success:function(a) {
-    CollectPortalInfo(a)
-    h(a)
-  }, error:function(a, c) {
-    _gaq.push(["_trackEvent", "RPC_error", b]);
-    f && f({error:c || "unknown", respStatus:a.status})
-  }, complete:function(a) {
-    j(b, a)
-  }});
-  Yc(a, b).push(c)
-}
-*/
-
-window.Zc = function Zc(a, b, c, d, e, f) {
-  c.method = b;
-  var g = t(a.Gc, a, b, e), h = t(a.dd, a, b, d), j = t(a.gd, a);
-  _gaq.push(["_trackEvent", "RPC", b]);
-  c = $.ajax({type:"POST", url:"/rpc/" + b, dataType:"json", contentType:"application/json; charset=utf-8", data:JSON.stringify(c), Ad:function() {
-    g()
-  }, success:function(a) {
-    CollectPortalInfo(a)
-    h(a)
-  }, error:function(a, c) {
-    _gaq.push(["_trackEvent", "RPC_error", b]);
-    f && f({error:c || "unknown", respStatus:a.status})
-  }, complete:function(a) {
-    j(b, a)
-  }});
-  Yc(a, b).push(c)
-}
+eval(String(Zc.prototype.constructor).replace("success:function(a) {", "success:function(a) {\n    CollectPortalInfo(a)").replace("function Zc", "window.Zc = function"))
 
 //include datatables
 var script = document.createElement('script')
